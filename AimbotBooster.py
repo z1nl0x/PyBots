e44e41a4c6fc6422aa03f81fbbb225b36c22bbca
z1nl0x <paulo.kreft@gmail.com>
2023-02-12 12:36:52 -0300

    AimbotBooster update file

--- AimbotBooster.py
+++ AimbotBooster.py
@@ -1,3 +1,5 @@
+# ALL CREDITS TO KIAN BROSE -> YouTube Channel: https://www.youtube.com/@KianBrose
+
 # https://github.com/KianBrose/Image-Recognition-Botting-Tutorial
 # https://github.com/KianBrose/Image-Recognition-Botting-Tutorial/blob/master/aimbooster.py
 # How to create an enviroment variable: py -m venv variableName
@@ -14,13 +16,14 @@ import win32api, win32con
 
 time.sleep(2)
 
+# Função responsável por criar o movimento de apertar o botão esquerdo do Mouse
 # Function responsible to create the moviment of a LEFT MOUSE CLICK
 def click(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
-
+# Loop responsável por checar os pixels dentro de uma área especificada no código (660,350) e encontrar o RGB: (249, 220, 196) cor(b=196) e clicar em cima do mesmo.
 # Loop responsible to check the pixels inside of an especified area (660,350) and find the RGB: (249, 220, 196) color(b=196) and click on it.
 while keyboard.is_pressed('q') == False:
 
