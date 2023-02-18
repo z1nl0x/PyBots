b9e4f24da64fb7f87b5eddaaf03ac165d4e0d799
z1nl0x <paulo.kreft@gmail.com>
2023-02-18 16:39:44 -0300

    Open_CV first update

---
+++ OpenCV_Training/OpenCV_FCC_Doc.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+
+# Read Videos and Images with OpenCV
+
+# Reading an image
+# img = cv.imread('Photos/py_robotic_snake.png')
+
+# Loading the image in a Window
+# cv.imshow('Py Robotic Snake', img)
+
+# Reading Videos
+# capture = cv.VideoCapture('Videos/CHARGE-Movie.mp4')
+
+# while True:
+#     isTrue, frame = capture.read()
+#     cv.imshow('Video', frame)
+
+#     if cv.waitKey(20) & 0xFF==ord('d'):
+#         break
+
+# capture.release()
+# cv.destroyAllWindows()
+
+# Wait an infinite amount of time of a Key to be pressed
+# cv.waitKey(0)
+
+
+# =====================================================================================================
+
+# Resizing and Rescaling
+
